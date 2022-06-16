9e8d3dbf04690c40a66620a1a149b5c9e2b592d1
Wesley Bernardes <bw3sley@gmail.com>
2022-06-16 15:35:47 -0300

    :sparkles: feat: create HTML/CSS structure

--- api.js
+++ api.js
@@ -1,6 +1,4 @@
-// Substitua o CHAVE_DA_API com a chave gerada no site.
-
-export const API_KEY = 'api_key=CHAVE_DA_API';
+export const API_KEY = 'api_key=382eb5d29663be039dbe04d839dcf0c8';
 export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 export const language = 'language=pt-BR';
